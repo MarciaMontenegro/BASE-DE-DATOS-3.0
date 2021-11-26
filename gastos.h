4ea2ae5dc66a2f6d21346307a9a2a7c4e1f7b660
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:46:43 -0500

    Update gastos.h

--- gastos.h
+++ gastos.h
@@ -1,17 +1,17 @@
 #ifndef GASTOS_H
 #define GASTOS_H
-
-#include <iostream>
-
-class Gasto {
-public:
-	std::string descripcion;
-	std::string area;
-	int codigo;
-
-	Gasto();
-	Gasto(std::string descripcion, int codigo, std::string area);
-	void info();
+#include<iostream>
+class gastos{
+	public:
+		std::string descripcion;
+		std::string area;
+		int gastoID;
+		gastos();
+		gastos(std::string descripcion, std::string area);
+		~gastos();
+		void info();
+		int set_gastoID(int gastoID);
+	protected:
 };
 
 #endif
