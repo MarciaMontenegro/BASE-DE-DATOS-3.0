c12e5eb80d5fbf97f886c07c9a7f0f48fce1c19a
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:45:48 -0500

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,54 +1,62 @@
 #ifndef MENU_H
 #define MENU_H
 #include <iostream>
-#include <string>
+#include "persona.h"
 #include "cliente.h"
-#include "cuarto.h"
 #include "empleado.h"
 #include "gastos.h"
+#include "cuarto.h"
 
-class menu {
-public:
-	std::string fecha;
-	bool principall;
-	menu(std::string fecha, bool principall);
-	~menu();
+class menu{
+	public:
+		std::string fecha;
+		bool bucle_main;
+		int l_clientes=10, l_empleados=10, l_cuartos=10, l_gastos=10; //EL NUMERO 10 SERA REEMPLAZADO POR EL VALOR POR LEER EN EL ARCHVO .TXT
+		
+		menu(std::string fecha, bool bucle_main);
+		~menu();
 
-	void mostrar_menu_voucher();
-	void mostrar_menu_eliminar();
-	void mostrar_menu_datos();
-	void mostrar_menu_creadores();
-	void mostrar_menu_modificaciones();
-	void mostrar_menu_principal();
+		//ASIGNACION DE MEMORIA DINAMICA
+		cliente* ptr_clientes = new cliente[l_clientes]; //ptr_clientes[i];	
+		empleado* ptr_empleados = new empleado[l_empleados]; //ptr_empleados[i]
+		cuarto* ptr_cuartos = new cuarto[l_cuartos]; // ptr_cuartos[i]
+		gastos* ptr_gastos = new gastos[l_gastos]; // ptr_gastos[i]
+		
+		
+		void mostrar_menu_voucher();
+		void mostrar_menu_eliminar();
+		void mostrar_menu_datos();
+		void mostrar_menu_creadores();
+		void mostrar_menu_principal();
+		void mostrar_menu_gastos();
+		
+		
+		//FUNCIONES MENU CREADORES
+		void nuevo_cliente();
+		void nuevo_empleado();
+		void nuevo_cuarto();
+		void nuevo_gasto();
 
+		//FUNCIONES MENU MOSTRAR DATOS
+		void mostrar_clientes();
+		void mostrar_empleados();
+		void mostrar_cuartos();
+		void mostrar_gastos();
 
-	//FUNCIONES MENU CREADORES
-	void CrearCliente();
-	void CrearEmpleado();
-	void CrearCuarto();
-	void CrearGasto();
+		//FUNCIONES MENU ELIMINAR
+		void eliminar_cliente();
+		void eliminar_empleado();
+		void eliminar_cuarto();
+		void eliminar_gastos();
 
-	//FUNCIONES MENU MOSTRAR DATOS
-	void MostrarClientes();
-	void MostrarEmpleados();
-	void MostrarCuartos();
-	void MostrarGastos();
-
-	//FUNCIONES MENU ELIMINAR
-	void EliminarCliente();
-	void EliminarEmpleado();
-	void EliminarCuarto();
-	void EliminarGastos();
-
-	//FUNCIONES MENU VOUCHER
-	void CrearVoucher();
-	void MostrarVoucher();
-
-
-
-
-
-protected:
+		//FUNCIONES MENU VOUCHER
+		void crear_voucher();
+		void mostrar_voucher();
+		
+		void guardar_datos();
+		
+		
+	protected:
 };
 
 #endif
