520df3cf18d0301ebfbd3e9edd5397b8820b059a
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:48:18 -0500

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,19 +1,20 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
-#include <iostream>
 #include "persona.h"
 
-
-class Cliente : public Persona {
-public:
-	std::string nombre, rango;
-	int DNI, telefono, check_in, check_out, num_reserva;
-	float pago;
-	Cliente();
-	Cliente(std::string nombre, int DNI, int telefono, std::string rango, int check_in, int check_out, int num_reserva, float pago);
-	void info();
-	void CrearCliente();
+class cliente : public persona{
+	public:
+		int clienteID;
+		std::string nombre,apellido, rango;
+		int DNI, telefono, check_in, check_out, num_reserva;
+		float pago;
+		cliente();
+		cliente(std::string nombre,std::string apellido, int DNI, int telefono, std::string rango, int check_in, int check_out, int num_reserva, float pago);
+		~cliente();
+		int set_clienteID(int clienteID);
+		void info();
+	protected:
 };
 
 #endif
