6681091e4c6a85cb0114070f68d3328f48f97e38
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:46:21 -0500

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,315 +1,322 @@
 #include "menu.h"
 #include <conio.h>
 #include <Windows.h>
-menu::menu(std::string _fecha, bool _principall) {
-	fecha = _fecha;
-	principall = _principall;
+#include <fstream>
+
+menu::menu(std::string _fecha, bool _bucle_main){
+	fecha=_fecha;
+	bucle_main=_bucle_main;
+	
 }
 
-menu::~menu() {
+menu::~menu(){
+	
 }
 
-void menu::mostrar_menu_voucher() {
-	std::cout << "Menu de datos: " << std::endl;
-	std::cout << "1.- Imprimir voucher" << std::endl;
-	std::cout << "4.- Volver al menu principal" << std::endl;
-	switch (_getch()) {
-	case '1':
-		std::cout << "\n Imprimiendo voucher\n";
-		break;
-
-	case '4':
-		std::cout << "\nVolviendo...\n";
-		mostrar_menu_principal();
-		break;
-	default:
-		break;
+void menu::eliminar_cliente(){
+	int buscador;
+	std::cout<<"ID de cliente a eliminar: ";
+	std::cin>>buscador;
+	for(int i=0; i<10; i++){
+		if(ptr_clientes[i].clienteID==buscador){
+			ptr_clientes[i].~cliente();
+			break;
+		}
 	}
 }
 
-void menu::mostrar_menu_eliminar() {
-	std::cout << "Menu de eliminación: " << std::endl;
-	std::cout << "1.- Eliminar cliente" << std::endl;
-	std::cout << "2.- Eliminar empleado" << std::endl;
-	std::cout << "3.- Eliminar cuartos" << std::endl;
-	std::cout << "4.- Volver al menu principal" << std::endl;
-	switch (_getch()) {
-	case '1':
-		std::cout << "\n Implementar el destructor de cliente\n";
-		break;
-	case '2':
-		std::cout << "\n Implementar el destructor de empleado\n";
-		break;
-	case '3':
-		std::cout << "\n Implementar el destructor de cuartos\n";
-		break;
-	case '4':
-		std::cout << "\nVolviendo...\n";
-		mostrar_menu_principal();
-		break;
-	default:
-		break;
+void menu::eliminar_empleado(){
+	int buscador;
+	std::cout<<"ID de empleado a eliminar: ";
+	std::cin>>buscador;
+	for(int i=0; i<10; i++){
+		if(ptr_empleados[i].empleadoID==buscador){
+			ptr_empleados[i].~empleado();
+			break;
+		}
 	}
 }
 
-void menu::mostrar_menu_datos() {
-	std::cout << "Menu de datos: " << std::endl;
-	std::cout << "1.- Ver clientes" << std::endl;
-	std::cout << "2.- Ver empleados" << std::endl;
-	std::cout << "3.- Ver cuartos" << std::endl;
-	std::cout << "4.- Ver gastos" << std::endl;
-	std::cout << "5.- Volver al menu principal" << std::endl;
-	switch (_getch()) {
-	case '1':
-		MostrarClientes();
-		break;
-	case '2':
-		MostrarEmpleados();
-		break;
-	case '3':
-		MostrarCuartos();
-		break;
-	case '4':
-		MostrarGastos();
-	case '5':
-		std::cout << "\nVolviendo...\n";
-		mostrar_menu_principal();
-		break;
-	default:
-		break;
+void menu::eliminar_cuarto(){
+	int buscador;
+	std::cout<<"ID de empleado a eliminar: ";
+	std::cin>>buscador;
+	for(int i=0; i<10; i++){
+		if(ptr_cuartos[i].cuartoID==buscador){
+			ptr_cuartos[i].~cuarto();
+			break;
+		}
 	}
 }
 
-void menu::mostrar_menu_creadores() {
-	std::cout << "Menu de creadores: " << std::endl;
-	std::cout << "1.- Nuevo cliente" << std::endl;
-	std::cout << "2.- Nuevo empleado" << std::endl;
-	std::cout << "3.- Nuevo cuarto" << std::endl;
-	std::cout << "4.- Nuevo gasto" << std::endl;
-	std::cout << "5.- Volver al menu principal" << std::endl;
-	switch (_getch()) {
-	case '1':
-		CrearCliente();
-		break;
-	case '2':
-		CrearEmpleado();
-		break;
-	case '3':
-		CrearCuarto();
-		break;
-	case '4':
-		CrearGasto();
-	case '5':
-		std::cout << "\nVolviendo...\n";
-		mostrar_menu_principal();
-		break;
-	default:
-		break;
+void menu::eliminar_gastos(){
+}
+
+void menu::mostrar_menu_gastos(){
+}	
+
+void menu::mostrar_menu_voucher(){
+	std::cout<<"Menu de datos: "<<std::endl;
+	std::cout<<"1.- Imprimir voucher"<<std::endl;
+	std::cout<<"4.- Volver al menu principal"<<std::endl;
+	switch(_getch()){
+		case '1':
+			std::cout<<"\n Imprimiendo voucher\n";
+			break;
+
+		case '4':
+			std::cout<<"\nVolviendo...\n";
+			mostrar_menu_principal();
+			break;
+		default:
+			break;
 	}
 }
 
-void menu::mostrar_menu_modificaciones() {
-	std::cout << "Menu para modificar datos: " << std::endl;
-	std::cout << "1.- Modificar datos de clientes" << std::endl;
-	std::cout << "2.- Modificar datos de empleados" << std::endl;
-	std::cout << "3.- Modificar datos de cuartos" << std::endl;
-	std::cout << "4.- Modificar datos de gastos" << std::endl;
-	std::cout << "5.- Volver al menu principal" << std::endl;
-	switch (_getch()) {
-	case '1':
-		std::cout << "\n Implementar modificador de cliente\n";
-		break;
-	case '2':
-		std::cout << "\n Implementar modificador de empleados\n";
-		break;
-	case '3':
-		std::cout << "\n Implementar modificador de cuartos\n";
-		break;
-	case '4':
-		std::cout << "\n Implementar modificador de gastos\n";
-	case '5':
-		std::cout << "\nVolviendo...\n";
-		mostrar_menu_principal();
-		break;
-	default:
-		break;
+void menu::mostrar_menu_eliminar(){
+	std::cout<<"Menu de eliminación: "<<std::endl;
+	std::cout<<"1.- Eliminar cliente"<<std::endl;
+	std::cout<<"2.- Eliminar empleado"<<std::endl;
+	std::cout<<"3.- Eliminar cuartos"<<std::endl;
+	std::cout<<"4.- Volver al menu principal"<<std::endl;
+	switch(_getch()){
+		case '1':
+			eliminar_cliente();
+			break;
+		case '2':
+			eliminar_empleado();
+			break;
+		case '3':
+			eliminar_cuarto();
+			break;
+		case '4':
+			std::cout<<"\nVolviendo...\n";
+			mostrar_menu_principal();
+			break;
+		default:
+			break;
 	}
 }
 
-void menu::mostrar_menu_principal() {
-	std::cout << "Fecha de hoy: " << fecha << std::endl;
-	std::cout << "Bienvenido a la interfaz";
-	std::cout << "\n1.- Creadores";
-	std::cout << "\n2.- Ver personas o cuartos";
-	std::cout << "\n3.- Eliminar clientes, empleados o cuartos";
-	std::cout << "\n4.- Modificar datos";
-	std::cout << "\n5.- Voucher";
-	std::cout << "\n6.- Gastos";
-	switch (_getch()) {
-	case '1':
-		std::cout << "\nCreadores\n";
-		mostrar_menu_creadores();
-		break;
-	case '2':
-		std::cout << "\nVer datos\n";
-		mostrar_menu_datos();
-		break;
-	case '3':
-		std::cout << "\nEliminar clientes, empleados o cuartos\n";
-		mostrar_menu_eliminar();
-		break;
-	case '4':
-		std::cout << "\nModificar datos";
-		mostrar_menu_modificaciones();
 
-	case '5':
-		std::cout << "\nVoucher\n";
-		mostrar_menu_voucher();
-		break;
-	case '6':
-		std::cout << "\nGastos\n";
-		std::cout << "\nAun por implementar\n";
-		break;
+void menu::mostrar_menu_datos(){
+	std::cout<<"Menu de datos: "<<std::endl;
+	std::cout<<"1.- Ver clientes"<<std::endl;
+	std::cout<<"2.- Ver empleados"<<std::endl;
+	std::cout<<"3.- Ver cuartos"<<std::endl;
+	std::cout<<"4.- Volver al menu principal"<<std::endl;
+	switch(_getch()){
+		case '1':
+			mostrar_clientes();
+			break;
+		case '2':
+			mostrar_empleados();
+			break;
+		case '3':
+			mostrar_cuartos();
+			break;
+		case '4':
+			std::cout<<"\nVolviendo...\n";
+			mostrar_menu_principal();
+			break;
+		default:
+			break;
+	}
+}
 
-	case '0':
-		std::cout << "\nTerminando proceso\n";
-		principall = false;
-		break;
-	default:
-		break;
+void menu::mostrar_menu_creadores(){
+	std::cout<<"Menu de creadores: "<<std::endl;
+	std::cout<<"1.- Nuevo cliente"<<std::endl;
+	std::cout<<"2.- Nuevo empleado"<<std::endl;
+	std::cout<<"3.- Nuevo cuarto"<<std::endl;
+	std::cout<<"4.- Nuevo gasto"<<std::endl;
+	std::cout<<"4.- Volver al menu principal"<<std::endl;
+	switch(_getch()){
+		case '1':
+			nuevo_cliente();
+			break;
+		case '2':
+			nuevo_empleado();
+			break;
+		case '3':
+			nuevo_cuarto();
+			break;
+		case '4':
+			mostrar_menu_principal();
+			break;
+		default:
+			break;
+	}
+}
+
+void menu::mostrar_menu_principal(){
+	std::cout<<"Fecha de hoy: "<<fecha<<std::endl;
+	std::cout<<"Bienvenido a la interfaz";
+	std::cout<<"\n1.- Creadores";
+	std::cout<<"\n2.- Ver personas o cuartos";
+	std::cout<<"\n3.- Eliminar clientes, empleados o cuartos";
+	std::cout<<"\n4.- Voucher";
+	std::cout<<"\n5.- Gastos"<<std::endl;
+	switch(_getch()){
+		case '1':
+			mostrar_menu_creadores();
+			break;
+		case '2':
+			mostrar_menu_datos();
+			break;
+		case '3':
+			mostrar_menu_eliminar();
+			break;
+		case '4':
+			mostrar_menu_voucher();
+			break;
+		case '5':
+			mostrar_menu_gastos();
+			break;
+			
+		case '0':
+			std::cout<<"\nTerminando proceso\n";
+			bucle_main=false;
+			break;
+		default:
+			break;
 	};
-
-
 }
 
-Cliente Clientes[10];
 //FUNCIONES MENU CREADORES
 
-void menu::CrearCliente() {
-	std::string a, b;
-	int x, y, z, w, o;
-	float p;
-	std::cout << "Nombre del cliente: "; 
-	getline(std::cin, a);
-	std::cout << "DNI: "; std::cin >> x;
-	std::cout << "Telefono: "; std::cin >> y;
-	std::cout << "Nivel: "; std::cin >> b;
+void menu::nuevo_cliente(){
+	//sumamos 1 al ptr_clientes
+ std::string nombre, apellido, nivel;
+  
+  	int dni, cel, ingreso, salida, reserva;
+	float pago;
+	
+	std::cout << "Nombre del cliente: "; std::cin >> nombre;
+	std::cout << "Apellido del cliente: "; std::cin >> apellido;
+	std::cout << "DNI: "; std::cin >> dni;
+	std::cout << "Telefono: "; std::cin >> cel;
+	std::cout << "Nivel: "; std::cin >> nivel;
+	std::cout << "Fecha de ingreso: (DDMMAAAA)"; std::cin >> ingreso;
+	std::cout << "Fecha de salida: (DDMMAAAA)"; std::cin >> salida;
+	std::cout << "Numero de habitacion de la reserva "; std::cin >> reserva;
+	std::cout << "Pago por adelantado "; std::cin >> pago;
+	
 
-	std::cout << "Fecha de ingreso: (DDMMAAAA)"; std::cin >> z;
-	std::cout << "Fecha de salida: (DDMMAAAA)"; std::cin >> w;
-	std::cout << "Numero de habitacion de la reserva "; std::cin >> o;
-	std::cout << "Pago por adelantado "; std::cin >> p;
-
-
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < l_clientes; i++)
 	{
-		if (Clientes[i].nombre == "")
+		if (ptr_clientes[i].nombre == "")
 		{
-			Clientes[i] = Cliente(a, x, y, b, z, w, o, p);
+			ptr_clientes[i]= cliente(nombre, apellido, dni,cel,nivel,ingreso,salida,reserva, pago);
+			ptr_clientes[i].set_clienteID(i);
 			break;
 		}
 	}
 }
 
-
-Empleado Empleados[10];
-void menu::CrearEmpleado() {
-	std::string a, b; 
-	int x, y, z, w, o; 
+void menu::nuevo_empleado(){
+	std::string nombre, apellido,area;
+	int dni,cel,hentrada,hsalida;
 	float s;
-	std::cout << "Nombre del empleado: "; getline(std::cin, a); 
-	std::cout << "DNI: "; std::cin >> x;
-	std::cout << "Telefono: "; std::cin >> y;
+	std::cout << "Nombre del empleado: "; std::cin>>nombre;
+	std::cout << "Apellido del empleado: "; std::cin>>apellido;
+	std::cout << "DNI: "; std::cin >> dni;
+	std::cout << "Telefono: "; std::cin >> cel;
 	std::cout << "Sueldo:"; std::cin >> s;
-	std::cout << "Area de trabajo:"; getline(std::cin, b);
-	std::cout << "Horario: Desde: "; std::cin >> w;
-	std::cout << " a "; std::cin >> o;
+	std::cout << "Area de trabajo:"; std::cin>>area;
+	std::cout << "Horario: Desde: "; std::cin >> hentrada;
+	std::cout << " a "; std::cin >> hsalida;
 
-	for (int i = 0; i < 10; i++) {
-		if (Empleados[i].nombre == "") {
-			Empleados[i] = Empleado(a, x, y, s, b, w, o);
+	for (int i = 0; i < l_empleados; i++) {
+		if (ptr_empleados[i].nombre == "") {
+			ptr_empleados[i] = empleado (nombre,apellido,dni,cel,s,area,hentrada,hsalida);
+			ptr_empleados[i].set_empleadoID(i);
 			break;
 		}
 	}
 }
 
-Cuarto Cuartos[10];
-void menu::CrearCuarto() {
+void menu::nuevo_cuarto(){
 	std::string tipo;
-	int numero;
 	int precio;
 	int estado;
 	int camas;
-	std::cout << "Tipo de habitacion: "; getline(std::cin, tipo); std::cout << std::endl;
-	std::cout << "Numero de habitacion: "; std::cin >> numero; std::cout << std::endl;
+	std::cout << "Tipo de habitacion: "; std::cin>>tipo; std::cout << std::endl;
 	std::cout << "Precio: "; std::cin >> precio; std::cout << std::endl;
 	std::cout << "Numero de camas: "; std::cin >> camas; std::cout << std::endl;
 	std::cout << "Estado de habitacion: "; std::cin >> estado; std::cout << std::endl;
-	for (int i = 0; i < 10; i++) {
-		if (Cuartos[i].estado == 0) {
-			Cuartos[i] = Cuarto(numero, precio, estado, camas, tipo);
+	for (int i = 0; i < l_cuartos; i++) {
+		if (ptr_cuartos[i].estado == 0) {
+			ptr_cuartos[i]= cuarto (precio, estado, camas, tipo);
 			break;
 		}
 	}
 }
 
-Gasto Gastos[10];
-void menu::CrearGasto() {
+void menu::nuevo_gasto(){
 	std::string descripcion;
-	int codigo;
 	std::string area;
 	std::cout << "Descripcion del gasto: "; getline(std::cin, descripcion);
 	std::cout << "Area en la que se requiere el gasto: "; getline(std::cin, area);std::cout << std::endl;
-	std::cout << "Codigo del gasto: "; std::cin >> codigo; std::cout << std::endl;
-
+	for (int i=0; i<l_gastos; i++){
+		if (ptr_gastos[i].area==""){
+			ptr_gastos[i].set_gastoID(i);
+			ptr_gastos[i]= gastos(descripcion, area);
+			break;
+		}
+	}
 }
 
 //FUNCIONES MENU MOSTRAR DATOS
 
-void menu::MostrarClientes() {
-	for (int i = 0; i < 10; i++) {
-		if (Clientes[i].nombre != "") {
+void menu::mostrar_clientes(){
+	for (int i = 0; i < l_clientes; i++) {
+		if (ptr_clientes[i].nombre != "") {
 			std::cout << "Cliente" << i << std::endl;
-			Clientes[i].info();
+			ptr_clientes[i].info();
 		}
 		std::cout << std::endl;
 	}
 }
 
-void menu::MostrarEmpleados() {
+void menu::mostrar_empleados(){
 	for (int i = 0; i < 10; i++) {
-		if (Empleados[i].nombre != "") {
+		if (ptr_empleados[i].nombre != "") {
 			std::cout << "Empleados	" << i << std::endl;
-			Empleados[i].info();
+			ptr_empleados[i].info();
 		}
 		std::cout << std::endl;
 	}
 }
 
-void menu::MostrarCuartos() {
+void menu::mostrar_cuartos(){
 	for (int i = 0; i < 10; i++) {
-		if (Cuartos[i].tipo != "") {
+		if (ptr_cuartos[i].precio != 0) {
 			std::cout << "Cuartos" << i << std::endl;
-			Cuartos[i].info();
+			ptr_cuartos[i].info();
 		}
 		std::cout << std::endl;
 	}
 }
 
-void menu::MostrarGastos() {
+void menu::mostrar_gastos(){
 	for (int i = 0; i < 10; i++) {
-		if (Gastos[i].descripcion != "") {
+		if (ptr_gastos[i].descripcion != "") {
 			std::cout << "Gastos" << i << std::endl;
-			Gastos[i].info();
+			ptr_gastos[i].info();
 			break;
 		}
 		std::cout << std::endl;
 	}
-
 }
 
 
-	
 
+
+
+
+void menu::guardar_datos(){
+
+}
 
 
