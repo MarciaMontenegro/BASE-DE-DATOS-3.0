f398445a192d1ac02ff8eafa97d7b3d4efc409b3
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:45:27 -0500

    Update persona.cpp

--- persona.cpp
+++ persona.cpp
@@ -1,16 +1,20 @@
 #include "persona.h"
 
-Persona::Persona(){
-  nombre = "";
-  DNI = 0;
-  telefono = 0;
+persona::persona(){
+	
 }
 
-Persona::Persona(std::string _nombre, int _DNI, int _telefono){
-	nombre=_nombre; DNI=_DNI; telefono=_telefono;
+persona::persona(std::string _nombre, int _DNI, int _telefono) {
+	nombre = _nombre;
+	DNI = _DNI;
+	telefono = _telefono;
 }
 
-void Persona::info(){
+
+persona::~persona(){
+}
+
+void persona::info() {
 	std::cout << "Nombre: " << nombre << std::endl;
 	std::cout << "DNI: " << DNI << std::endl;
 	std::cout << "Telefono: " << telefono << std::endl;
