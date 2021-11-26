d6a64f48500fb468d616671355dee98e4f2294a4
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:49:01 -0500

    Create voucher.h

---
+++ voucher.h
@@ -0,0 +1,15 @@
+#ifndef VOUCHER_H
+#define VOUCHER_H
+
+class voucher{
+	public:
+		int voucherID;
+		int cuarto;
+		int precio;
+		voucher();
+		voucher(int cuarto, int precio);
+		~voucher();
+	protected:
+};
+
+#endif
