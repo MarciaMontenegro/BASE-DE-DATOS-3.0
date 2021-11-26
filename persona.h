7845687562d16c466d77840b928384b6751d5fde
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:45:05 -0500

    Update persona.h

--- persona.h
+++ persona.h
@@ -1,15 +1,16 @@
 #ifndef PERSONA_H
 #define PERSONA_H
-#include <iostream>
-
-class Persona{
-  public:
-	std::string nombre;
-	int DNI;
-	int telefono;
-	Persona ();
-	Persona (std::string nombre, int DNI, int telefono);
-    	void info();
+#include<iostream>
+class persona{
+	public:
+		std::string nombre;
+		int DNI;
+		int telefono;
+		persona();
+		persona(std::string nombre, int DNI, int telefono);
+		~persona();
+		void info();
+	protected:
 };
 
 #endif
