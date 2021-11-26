1027090f397d6989a9b7a1a1ebd83008c0aab49c
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-26 09:47:14 -0500

    Update empleado.h

--- empleado.h
+++ empleado.h
@@ -1,19 +1,22 @@
 #ifndef EMPLEADO_H
 #define EMPLEADO_H
-
 #include "persona.h"
 
-class Empleado : public Persona{
-  public:
-	std::string nombre;
-	int DNI;
-	int telefono;
-	float sueldo;
-	std::string area;
-	int hora_e, hora_s;	
-    Empleado();	
-	Empleado(std::string nombre, int DNI, int telefono, float sueldo, std::string area, int hora_e, int hora_s);
-    void info();
+class empleado: public persona{
+	public:
+		int empleadoID;
+		std::string nombre,apellido;
+		int DNI;
+		int telefono;
+		float sueldo;
+		std::string area;
+		int hora_e, hora_s;
+		empleado();
+		empleado(std::string nombre,std::string apellido, int DNI, int telefono, float sueldo, std::string area, int hora_e, int hora_s);
+		~empleado();
+		void info();
+		int set_empleadoID(int empleadoID);
+	protected:
 };
 
 #endif
