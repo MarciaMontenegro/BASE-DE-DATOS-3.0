3d99767f1f7efd18a7029c653d40b5094f386496
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-28 18:31:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include <fstream>
 #include "menu.h"
 #include "MyLib.h"
-//VIDA LOCA
+//VIDA LOCA muy lloca
 bool bucle_main=true;
 
 int main(){
 	std::string f;	
 	std::cout<<"Introduzca la fecha del dia: "; std::cin>>f;
-	menu menu1(f, bucle_main);
+	menu menu1(f, bucle_main, cargar_datos_clientes(), cargar_datos_empleados(), cargar_datos_cuartos(), cargar_datos_gastos());
 	while (menu1.bucle_main){
 		menu1.mostrar_menu_principal();
+
 	}
 	return 0;
 }
