f731e78397391706dab4b1c5ca809274efc38c19
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-12-02 09:43:01 -0500

    Update cuarto.cpp
    mejora del id

--- cuarto.cpp
+++ cuarto.cpp
@@ -3,11 +3,13 @@
 cuarto::cuarto(){
 }
 
-cuarto::cuarto(int _precio, int _estado, int _camas, std::string _tipo){
+cuarto::cuarto(int _id, std::string _tipo, int _camas, int _precio,std::string _estado){
+	cuartoID = _id;
 	precio=_precio;
 	estado=_estado;
 	camas=_camas;
 	tipo=_tipo;
+	
 }
 
 cuarto::~cuarto(){
@@ -15,11 +17,10 @@ cuarto::~cuarto(){
 
 void cuarto::info() {
 	std::cout << "Numero de habitacion: " << cuartoID << std::endl;
-	std::cout << "Precio de la habitacion: " << precio << std::endl;
-	std::cout << "Camas: " << camas << std::endl;
 	std::cout << "Tipo de habitación: " << tipo << std::endl;
+	std::cout << "Camas: " << camas << std::endl;
+	std::cout << "Precio de la habitacion: " << precio << std::endl;
+	std::cout << "Estado: " << estado << std::endl;
+
 }
 
-void cuarto::set_cuartoID(int _cuartoID){
-	cuartoID=_cuartoID;
-}
