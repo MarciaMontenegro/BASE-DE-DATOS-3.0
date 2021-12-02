fba556bb6b91e755f125e20d32a3f2284a02351c
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-12-02 09:41:35 -0500

    Update MyLib.h
    referencia

--- MyLib.h
+++ MyLib.h
@@ -1,9 +1,10 @@
+//MyLib.h
 
-int cargar_datos_clientes();
+int cargar_datos_clientes(int &nuevo);
 
-int cargar_datos_empleados();
+int cargar_datos_empleados(int &nuevo);
 
-int cargar_datos_cuartos();
+int cargar_datos_cuartos(int &nuevo);
 
-int cargar_datos_gastos();
+int cargar_datos_gastos(int &nuevo);
 
