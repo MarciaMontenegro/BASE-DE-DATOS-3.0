9b09c57e92e9939fac3a715ef1e9c146e3257ec7
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-12-02 09:45:23 -0500

    implementación  de la pregunta cuantos datos va a ingresar hoy para reservar memoria.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,24 @@ bool bucle_main=true;
 
 int main(){
 	std::string f;	
-	std::cout<<"Introduzca la fecha del dia: "; std::cin>>f;
-	menu menu1(f, bucle_main, cargar_datos_clientes(), cargar_datos_empleados(), cargar_datos_cuartos(), cargar_datos_gastos());
+	std::cout << "==========================================================================================================\n";
+	std::cout << "                                            BIENVENIDO A LA BASE DE DATOS"; std::cout << "\n";
+	std::cout << "==========================================================================================================\n";
+	std::cout << "                                           NECESITAMOS LOS SIGUIENTES DATOS:"; std::cout << "\n";
+	std::cout << "==========================================================================================================\n";
+	int nuevos1,nuevos2,nuevos3,nuevos4;
+	std::cout <<"Cuantos clientes va a ingresar hoy"; std::cin >> nuevos1;
+	std::cout <<"Cuantos empleados va a ingresar hoy"; std::cin >> nuevos2;
+	std::cout <<"Cuantos cuartos va a ingresar hoy"; std::cin >> nuevos3;
+	std::cout <<"Cuantos gastos va a ingresar hoy"; std::cin >> nuevos4;
+	
+	menu menu1( bucle_main, cargar_datos_clientes(nuevos1), cargar_datos_empleados(nuevos2), cargar_datos_cuartos(nuevos3), cargar_datos_gastos(nuevos4));
+
+	//CARGAR ARCHIVOS A LA RAM
+	menu1.cargar_datos();
+				
 	while (menu1.bucle_main){
 		menu1.mostrar_menu_principal();
-
 	}
 	return 0;
 }
