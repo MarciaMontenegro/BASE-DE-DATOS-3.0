2ec8a139ec00f39dc040c18a89ca3f7032bf1fbd
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-12-02 09:43:36 -0500

    Update empleado.cpp

--- empleado.cpp
+++ empleado.cpp
@@ -1,9 +1,19 @@
 #include "empleado.h"
 
 empleado::empleado(){
+	empleadoID = 0;
+	nombre = "";
+	apellido = "";
+	DNI = 0;
+	telefono = 0;
+	sueldo = 0;
+	area = "";
+	hora_e = 0;
+	hora_s = 0;
 }
 
-empleado::empleado(std::string _nombre,std::string _apellido, int _DNI, int _telefono, float _sueldo, std::string _area, int _hora_e, int _hora_s) :persona(_nombre, _DNI, _telefono) {
+empleado::empleado(int _empleadoID, std::string _nombre,std::string _apellido, int _DNI, int _telefono, float _sueldo, std::string _area, int _hora_e, int _hora_s) :persona(_nombre, _DNI, _telefono) {
+	empleadoID=_empleadoID;
 	nombre = _nombre;
 	apellido = _apellido;
 	DNI = _DNI;
@@ -11,16 +21,16 @@ empleado::empleado(std::string _nombre,std::string _apellido, int _DNI, int _tel
 	sueldo = _sueldo;
 	area = _area;
 	hora_e = _hora_e;
+	hora_s = _hora_s;
 }
 
 empleado::~empleado(){
 }
 
-int empleado::set_empleadoID(int _empleadoID){
-	empleadoID=_empleadoID;
-}
+
 
 void empleado::info() {
+	std::cout << "ID : " << empleadoID << std::endl;
 	std::cout << "Nombre: " << nombre << std::endl;
 	std::cout << "Apellido: " << apellido << std::endl;
 	std::cout << "Documento de identificación: " << DNI << std::endl;
@@ -29,4 +39,3 @@ void empleado::info() {
 	std::cout << "Area en la que trabaja: " << area << std::endl;
 	std::cout << "Horario de " << hora_e << " a " << hora_s << std::endl;
 }
-
